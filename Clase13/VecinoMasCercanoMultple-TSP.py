b3f757d80c572e94f8f8df79d802b287594cad1d
gcarvajal-sjc <gloriacarvajal@msn.com>
2021-11-26 12:50:03 -0800

    Algoritmo vecino mas cercano completo MTSP

--- Clase13/VecinoMasCercanoMultple-TSP.py
+++ Clase13/VecinoMasCercanoMultple-TSP.py
@@ -1,41 +1,7 @@
 # Requerimiento: Recibimos una red o grafo y se requiere obtener la matriz
 # de distancias correspondientes para computos de optimizacion.
 
-# Altenativas de codificacion
-
-# Ejemplos de coleecion
-# 1. Lista de listas representado la red:
-# - La lista general es la red, y cada posicion de la lista son los nodos
-
-red = [
-    ['A', 8, 11],
-    ['B', 7, 8],
-    ['C', 9, 5.5],
-    ['D', 9, 7],
-    ['E', 10, 10]
-]
-
-# 2. En la primera posicion de cada una de las listas esta la etiqueta y
-# las coordenadas en una tupla para que no cambien
-red = [
-    ['A', (8, 11)],
-    ['B', (7, 8)],
-    ['C', (9, 5.5)],
-    ['D', (9, 7)],
-    ['E', (10, 10)]
-]
-
-# 3. Una lista de tuplas
-red = [
-    ('A', (8, 11)),
-    ('B', (7, 8)),
-    ('C', (9, 5.5)),
-    ('D', (9, 7)),
-    ('E', (10, 10))
-]
-
-# 4. Tal vez no necesitamos el autonumerico. Entonces el diccionario permite
-# personalizar la etiqueta
+# Distancia euclidiana (2D)
 
 red = {
     'A': (8, 11),
@@ -44,9 +10,6 @@ red = {
     'D': (9, 7),
     'E': (10, 10)
 }
-
-
-# Distancia euclidiana (2D)
 
 
 def distanciaEUC_2D(punto1, punto2):
@@ -112,13 +75,44 @@ print('--------------')
 print(matrizCostos)
 
 # Consultar la estrategia del vecino mas cerano e intentar implementarla
-#sobre esta codificacion del problema
+# sobre esta codificacion del problema
 
-#Algoritmo o pseudocodigo del vecino mas cercano
-1. Establecer nodo o ciudad de partida
-2. Iniciar el itinerario o el tour en la ciudad de partida establecida
-3. Mientras tengamos ciudades sin cubrir en el itinerario:
-4.     Desde la ultima ciudad del itinerario revisamos las salidas a las demas ciudades
-5.     Seleccionar la mejor de las salidas
-6      Actualizar las ciudades sin cubrir 
-7.     Agregar al itinerario la mejor de las salidas
+# #Algoritmo o pseudocodigo del vecino mas cercano
+# 1. Establecer nodo o ciudad de partida
+# 2. Iniciar el itinerario o el tour en la ciudad de partida establecida
+# 3. Mientras tengamos ciudades sin cubrir en el itinerario:
+# 4.     Desde la ultima ciudad del itinerario revisamos las salidas a las demas ciudades
+# 5.     Seleccionar la mejor de las salidas
+# 6      Actualizar las ciudades sin cubrir
+# 7.     Agregar al itinerario la mejor de las salidas
+# 8.     Agregar al itinerario el regreso a la ciudad inicia o de partida
+
+# Traduccion a Python
+print("-----Algoritmo Heuristico del vecino mas cercano-----")
+# Inicializacion del tour y el conjunto de control de ciudades cubiertas
+ciudadInicial = "Manizales"
+itinerario = list()
+itinerario.append(ciudadInicial)
+ciudadesSinCubrir = set(red.keys())
+ciudadesSinCubrir.remove(ciudadInicial)
+# Seccion principal del algoritmo (cubrir todas las ciudades como ciclo hamiltoniano)
+while len(ciudadesSinCubrir) > 0:
+    # Construir el listado con las posibles salidas desde la ultima ciudad ingresada en el itinerario
+    listadoSalidas = list()
+    for ciudadSalida in ciudadesSinCubrir:
+        listadoSalidas.append(
+            (ciudadSalida, matrizCostos[itinerario[-1]+'-'+ciudadSalida]))
+    listadoSalidas = tuple(listadoSalidas)
+    mejorSalida = listadoSalidas[0]
+    for salida in listadoSalidas:
+        if mejorSalida[1] > salida[1]:
+            mejorSalida = salida
+    # Actualizar el itinerario
+    itinerario.append(mejorSalida[0])
+    # Actualizar el conjunto (entrando en el intenerario de la ciudad ahora esta cubierta)
+    ciudadesSinCubrir.remove(mejorSalida[0])
+
+# Retorno a la ciudad base
+itinerario.append(ciudadInicial)
+
+print("Itinerario resultante: ", itinerario)
